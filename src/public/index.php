97a7c20854e32bfec0c0b8ae22bf9eb764f68adb
matheuslesbao <matheuss.lesbao@gmail.com>
2024-03-04 22:27:42 -0300

    projeto ja iniciado

---
+++ src/public/index.php
@@ -0,0 +1,37 @@
+<?php 
+require_once '../app/config/php-config.php';
+require_once '../vendor/autoload.php';
+?>
+
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="/assets/css/global.css">
+    <title>Pet-Diet</title>
+</head>
+<body>
+   <header>
+    <h1>Diet Pet</h1>
+    <p>Comparação de ração</p>
+   </header>
+   <main>
+    <section class="container-flex">
+      <form action="" method="post">
+      <div class="images-container">
+        <h3>Tutano Premium Plus</h3>
+        <img src="/assets/img/teste.jpg" alt="">
+        <button class="btn-basic">Teste</button>
+        </div>
+        <div>
+        <h3>Tutano Premium Plus</h3>
+        <img src="/assets/img/teste.jpg" alt="">
+        <button class="btn-basic">Teste
+        </button>
+        </div>
+      </form>
+    </section>
+   </main>
+</body>
+</html>
